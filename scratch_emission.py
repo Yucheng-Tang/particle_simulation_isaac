d354a31432f5313e981d0ead50b375fba23bc1ad
Zhehao Ma <hao842095547@qq.com>
2025-09-25 15:27:07 +0800

    Chore: format

--- scratch_emission.py
+++ scratch_emission.py
@@ -16,7 +16,7 @@ def emit_new_particles(
     emission_spread: float,
     emission_vel_spread: float,
     current_particles: int,
-    max_particles: int
+    max_particles: int,
 ):
     tid = wp.tid()
     if tid < current_particles or tid >= max_particles:
@@ -24,18 +24,21 @@ def emit_new_particles(
 
     rng = wp.rand_init(seed, tid)
     pos_noise = wp.vec3(wp.randn(rng), wp.randn(rng), wp.randn(rng)) * emission_spread
-    vel_noise = wp.vec3(wp.randn(rng), wp.randn(rng), wp.randn(rng)) * emission_vel_spread
+    vel_noise = (
+        wp.vec3(wp.randn(rng), wp.randn(rng), wp.randn(rng)) * emission_vel_spread
+    )
     pos = emission_pos + pos_noise
     vel = emission_vel + vel_noise
     positions[tid] = pos
     velocities[tid] = vel
+
 
 @wp.kernel
 def simulate(
     positions: wp.array(dtype=wp.vec3),
     velocities: wp.array(dtype=wp.vec3),
     sim_timestep: float,
-    mesh: wp.uint64
+    mesh: wp.uint64,
 ):
     tid = wp.tid()
 
@@ -49,7 +52,7 @@ def simulate(
     v = (xpred - x) * (1.0 / sim_timestep)
     x = xpred
 
-    max_dist = 1.5
+    max_dist = 1.0
 
     query = wp.mesh_query_point_sign_normal(mesh, xpred, max_dist)
     if query.result:
@@ -60,8 +63,9 @@ def simulate(
     positions[tid] = x
     velocities[tid] = v
 
+
 class Emission:
-    def __init__(self, stage_path='emission.usd'):
+    def __init__(self, stage_path="emission.usd"):
         self.rng = np.random.default_rng(7777)
         self.seed = 7777
         self.sim_timestep = 1.0 / 60.0
@@ -74,14 +78,16 @@ class Emission:
 
         self.current_particles = 0
         self.emission_counter = 0.0
-        self.emission_rate = 1_000     # particles per second
+        self.emission_rate = 1_000  # particles per second
         self.emission_pos = wp.vec3(0.0, 20.0, 0.0)
         self.emission_spread = 0.5
         self.emission_vel = wp.vec3(0.0, 0.0, 0.0)
         self.emission_vel_spread = 1.0
 
         # create collision mesh
-        usd_stage = Usd.Stage.Open(os.path.join(warp.examples.get_asset_directory(), "bunny.usd"))
+        usd_stage = Usd.Stage.Open(
+            os.path.join(warp.examples.get_asset_directory(), "bunny.usd")
+        )
         usd_geom = UsdGeom.Mesh(usd_stage.GetPrimAtPath("/root/bunny"))
         usd_scale = 10.0
         self.mesh = wp.Mesh(
@@ -100,12 +106,12 @@ class Emission:
         self.emission_counter += self.emission_rate * self.sim_timestep
 
         particles_to_emit = int(self.emission_counter)
-        self.emission_counter -= particles_to_emit      # account for the remainder
+        self.emission_counter -= particles_to_emit  # account for the remainder
 
         wp.launch(
-            kernel = emit_new_particles,
-            dim = self.current_particles + particles_to_emit,
-            inputs = [
+            kernel=emit_new_particles,
+            dim=self.current_particles + particles_to_emit,
+            inputs=[
                 self.positions,
                 self.velocities,
                 self.emission_pos,
@@ -114,8 +120,8 @@ class Emission:
                 self.emission_spread,
                 self.emission_vel_spread,
                 self.current_particles,
-                self.max_particles
-            ]
+                self.max_particles,
+            ],
         )
         self.current_particles += particles_to_emit
 
@@ -124,9 +130,14 @@ class Emission:
             self.emit_new_particles()
             if self.current_particles > 0:
                 wp.launch(
-                    kernel = simulate,
-                    dim = self.current_particles,
-                    inputs = [self.positions, self.velocities, self.sim_timestep, self.mesh.id]
+                    kernel=simulate,
+                    dim=self.current_particles,
+                    inputs=[
+                        self.positions,
+                        self.velocities,
+                        self.sim_timestep,
+                        self.mesh.id,
+                    ],
                 )
             self.sim_time += self.sim_timestep
 
@@ -142,14 +153,23 @@ class Emission:
                 colors=(0.35, 0.55, 0.9),
             )
             self.renderer.render_points(
-                name="points", points=self.positions.numpy(), radius=self.particle_radius, colors=(0.8, 0.3, 0.2)
+                name="points",
+                points=self.positions.numpy(),
+                radius=self.particle_radius,
+                colors=(0.8, 0.3, 0.2),
             )
             self.renderer.end_frame()
 
+
 if __name__ == "__main__":
     import argparse
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("--device", type=str, default=None, help="Override the default Warp device.")
+
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        "--device", type=str, default=None, help="Override the default Warp device."
+    )
     parser.add_argument("--num_frames", type=int, default=200)
 
     args = parser.parse_known_args()[0]
@@ -162,4 +182,3 @@ if __name__ == "__main__":
 
         if e.renderer:
             e.renderer.save()
-
